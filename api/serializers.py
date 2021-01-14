6fb7dea926e4790af47c330ee1f77da43dea0d34
mascDriver <diogoblibi@hotmail.com>
2021-01-14 17:37:42 -0300

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
+
+class CreateRoomSetializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
+
+
+class UpdateRoomSetializer(serializers.ModelSerializer):
+    code = serializers.CharField(validators=[])
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
